2cb8b32b0432882e1e526ec8ebbb9ca0a2929564
rustam <kpytu3ha@yandex.ru>
2019-03-25 23:54:31 +0500

    PrefixStringComparer

---
+++ Indexer.Tests/PrefixStringComparerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Indexer.Tests
+{
+    [TestFixture]
+    public class PrefixStringComparerTests
+    {
+        [TestCase("a", "b", -1)]
+        [TestCase("b", "a", 1)]
+        [TestCase("a", "a", 0)]
+        [TestCase("ab", "a", 0)]
+        [TestCase("a", "ab", -98)]
+        [TestCase("ababa", "aba", 0)]
+        public void TestCompares(string x, string y, int expectedResult)
+        {
+            var comparer = new PrefixStringComparer();
+
+            var result = comparer.Compare(x, y);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [TestCase(null, "a")]
+        [TestCase("a", null)]
+        [TestCase(null, null)]
+        public void Throw_ArgumentException_If_Passed_Values_Is_Null(string x, string y)
+        {
+            var comparer = new PrefixStringComparer();
+
+            Action compareAction = () => comparer.Compare(x, y);
+
+            compareAction.Should().Throw<ArgumentException>().And.Message.Should().Be("Passed string could not be null");
+        }
+    }
+}
---
+++ Indexer/PrefixStringComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexer
+{
+    public class PrefixStringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x == null || y == null)
+            {
+                throw new ArgumentException("Passed string could not be null");
+            }
+
+            if (y.Length > x.Length)
+            {
+                return string.Compare(x, y, StringComparison.Ordinal);
+            }
+
+            return x.StartsWith(y) ? 0 : string.Compare(x, y, StringComparison.Ordinal);
+        }
+    }
+}
