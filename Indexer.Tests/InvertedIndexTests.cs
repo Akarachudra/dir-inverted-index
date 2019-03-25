b3d2c89b1061f316565583f1efcebb0fc95ab3ff
rustam <kpytu3ha@yandex.ru>
2019-03-26 01:10:55 +0500

    Broken InvertedIndex

---
+++ Indexer.Tests/InvertedIndexTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Indexer.Tests
+{
+    [TestFixture]
+    public class InvertedIndexTests
+    {
+        [Test]
+        public void Can_Add_To_Index_And_Find_Matches()
+        {
+            const string path = "file_path";
+            const string word = "te_st";
+            var invertedIndex = new InvertedIndex();
+            var searchResult = new SearchResult { RowNumber = 2, ColNumber = 1, FilePath = path };
+
+            invertedIndex.Add(word, searchResult);
+
+            invertedIndex.Find(word).Should().BeEquivalentTo(searchResult);
+            invertedIndex.Find("te_s").Should().BeEquivalentTo(searchResult);
+            invertedIndex.Find("e_st").Should().BeEquivalentTo(new SearchResult { RowNumber = 2, ColNumber = 2, FilePath = path });
+            invertedIndex.Find("e_s").Should().BeEquivalentTo(new SearchResult { RowNumber = 2, ColNumber = 2, FilePath = path });
+            invertedIndex.Find("some_word").Should().BeEmpty();
+        }
+    }
+}
---
+++ Indexer/InvertedIndex.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Indexer
+{
+    public class InvertedIndex
+    {
+        private readonly SortedList<string, IList<SearchResult>> list;
+
+        public InvertedIndex()
+        {
+            this.list = new SortedList<string, IList<SearchResult>>();
+        }
+
+        public void Add(string word, SearchResult curResult)
+        {
+            var length = word.Length;
+            for (var i = 0; i < word.Length; i++)
+            {
+                curResult = new SearchResult
+                {
+                    ColNumber = curResult.ColNumber + i,
+                    FilePath = curResult.FilePath,
+                    RowNumber = curResult.RowNumber
+                };
+
+                var suffix = word.Substring(i, length - i);
+                if (!this.list.ContainsKey(suffix))
+                {
+                    this.list.Add(suffix, new List<SearchResult>());
+                }
+
+                this.list[suffix].Add(curResult);
+            }
+        }
+
+        public IList<SearchResult> Find(string query)
+        {
+            if (this.list.ContainsKey(query))
+            {
+                return this.list[query];
+            }
+
+            return new List<SearchResult>();
+        }
+    }
+}
