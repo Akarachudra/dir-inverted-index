56dcdc9cf1a9b9c6756df99ce8ad7d8f8138fd61
rustam <kpytu3ha@yandex.ru>
2019-03-26 19:24:06 +0500

    SuffixArray impl. Tests

---
+++ Indexer.Tests/Collections/SuffixArrayTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Indexer.Collections;
+using Moq;
+using NUnit.Framework;
+
+namespace Indexer.Tests.Collections
+{
+    [TestFixture]
+    public class SuffixArrayTests
+    {
+        [Test]
+        public void Compare_Via_Insert_Comparer_On_Add()
+        {
+            var insertComparerMock = new Mock<IComparer<string>>();
+            var readComparerMock = new Mock<IComparer<string>>();
+            var suffixArray = new SuffixArray<string, int>(insertComparerMock.Object, readComparerMock.Object);
+
+            suffixArray.TryAdd("test", 1);
+            suffixArray.TryAdd("tes", 2);
+
+            insertComparerMock.Verify(x => x.Compare("test", "tes"), Times.Once);
+            readComparerMock.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public void Compare_Via_Read_Comparer_On_Get()
+        {
+            var insertComparerMock = new Mock<IComparer<string>>();
+            var readComparerMock = new Mock<IComparer<string>>();
+            var suffixArray = new SuffixArray<string, int>(insertComparerMock.Object, readComparerMock.Object);
+
+            suffixArray.TryAdd("test", 1);
+            suffixArray.TryAdd("tes", 2);
+
+            suffixArray.TryGetValue("tes", out _);
+
+            readComparerMock.Verify(x => x.Compare("test", "tes"), Times.Once);
+        }
+
+        [Test]
+        public void Keys_And_Values_Are_Ordered_After_Insert()
+        {
+            const string word = "ananas";
+            var expectedOrderedKeys = new[]
+            {
+                "ananas", "anas", "as", "nanas", "nas", "s"
+            };
+            var expectedOrderedValues = new[]
+            {
+                0, 2, 4, 1, 3, 5
+            };
+            var suffixArray = new SuffixArray<string, int>(StringComparer.Ordinal, StringComparer.Ordinal);
+            for (var i = 0; i < word.Length; i++)
+            {
+                var suffix = word.Substring(i, word.Length - i);
+                suffixArray.TryAdd(suffix, i);
+            }
+
+            suffixArray.Keys.Should().BeEquivalentTo(expectedOrderedKeys, options => options.WithStrictOrdering());
+            suffixArray.Values.Should().BeEquivalentTo(expectedOrderedValues, options => options.WithStrictOrdering());
+        }
+
+        [Test]
+        public void Can_Add_And_Get()
+        {
+            var suffixArray = new SuffixArray<string, int>(StringComparer.Ordinal, StringComparer.Ordinal);
+            int value;
+
+            suffixArray.TryAdd("test", 1).Should().BeTrue();
+            suffixArray.TryAdd("test", 1).Should().BeFalse();
+
+            suffixArray.TryGetValue("test", out value).Should().BeTrue();
+            value.Should().Be(1);
+            suffixArray.TryGetValue("not_exists_key", out value).Should().BeFalse();
+        }
+    }
+}
---
+++ Indexer/Collections/SuffixArray.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexer.Collections
+{
+    public class SuffixArray<TKey, TValue>
+    {
+        private const int MinimumCapacity = 16;
+        private readonly IComparer<TKey> insertComparer;
+        private readonly IComparer<TKey> readComparer;
+        private TKey[] keys;
+        private TValue[] values;
+        private int size;
+
+        public SuffixArray(IComparer<TKey> insertComparer, IComparer<TKey> readComparer)
+            : this(0, insertComparer, readComparer)
+        {
+        }
+
+        public SuffixArray(int capacity, IComparer<TKey> insertComparer, IComparer<TKey> readComparer)
+        {
+            this.insertComparer = insertComparer;
+            this.readComparer = readComparer;
+            this.keys = new TKey[0];
+            this.values = new TValue[0];
+            this.Capacity = capacity;
+        }
+
+        public TKey[] Keys
+        {
+            get
+            {
+                var keyArray = new TKey[this.size];
+                if (this.size > 0)
+                {
+                    Array.Copy(this.keys, 0, keyArray, 0, this.size);
+                }
+
+                return keyArray;
+            }
+        }
+
+        public TValue[] Values
+        {
+            get
+            {
+                var valueArray = new TValue[this.size];
+                if (this.size > 0)
+                {
+                    Array.Copy(this.values, 0, valueArray, 0, this.size);
+                }
+
+                return valueArray;
+            }
+        }
+
+        public int Capacity
+        {
+            get => this.keys.Length;
+
+            set
+            {
+                if (value < this.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Small capacity");
+                }
+
+                if (value != this.keys.Length)
+                {
+                    if (value > 0)
+                    {
+                        var newKeysArray = new TKey[value];
+                        var newValuesArray = new TValue[value];
+                        if (this.size > 0)
+                        {
+                            Array.Copy(this.keys, 0, newKeysArray, 0, this.size);
+                            Array.Copy(this.values, 0, newValuesArray, 0, this.size);
+                        }
+
+                        this.keys = newKeysArray;
+                        this.values = newValuesArray;
+                    }
+                    else
+                    {
+                        this.keys = new TKey[0];
+                        this.values = new TValue[0];
+                    }
+                }
+            }
+        }
+
+        public int Count => this.size;
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var index = Array.BinarySearch(this.keys, 0, this.size, key, this.readComparer);
+            if (index >= 0)
+            {
+                value = this.values[index];
+                return true;
+            }
+
+            value = default(TValue);
+            return false;
+        }
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            var index = Array.BinarySearch(this.keys, 0, this.size, key, this.insertComparer);
+            if (index >= 0)
+            {
+                return false;
+            }
+
+            index = ~index;
+
+            if (this.size == this.keys.Length)
+            {
+                this.EnsureCapacity(this.size + 1);
+            }
+
+            if (index < this.size)
+            {
+                Array.Copy(this.keys, index, this.keys, index + 1, this.size - index);
+                Array.Copy(this.values, index, this.values, index + 1, this.size - index);
+            }
+
+            this.size++;
+            this.keys[index] = key;
+            this.values[index] = value;
+
+            return true;
+        }
+
+        private void EnsureCapacity(int minimumSize)
+        {
+            var newCapacity = this.keys.Length == 0 ? MinimumCapacity : this.keys.Length * 2;
+            if ((uint)newCapacity > int.MaxValue)
+            {
+                newCapacity = int.MaxValue;
+            }
+
+            if (newCapacity < minimumSize)
+            {
+                newCapacity = minimumSize;
+            }
+
+            this.Capacity = newCapacity;
+        }
+    }
+}
